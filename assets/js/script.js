ab878d21d222d46b426c1ed7b2e999875f6fa14a
Sabrina Belloni <sabrina82belloni@gmail.com>
2024-07-22 16:44:45 +0000

    create skeleton function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,31 @@
-console.log('done')
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+    
+}
